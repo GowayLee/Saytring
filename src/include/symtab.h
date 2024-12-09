899cfe346eeb8b5b03a79919c9fc678779f6697f
Li Haoyuan <1513624626@qq.com>
2024-12-09 17:11:43 +0800

    feat: Add symbol table

--- src/include/symtab.h
+++ src/include/symtab.h
@@ -3,21 +3,31 @@
 
 #include <iostream>
 #include <string.h>
+#include <vector>
 
 class Symbol {
 protected:
   char *str; // the string
 public:
   Symbol(char *s);
-
-  int check;
-
-  // std::ostream &print(std::ostream &s) const;
-
   bool operator==(Symbol other);
 
   // Return the str and len components of the Entry.
   char *get_string() const { return str; }
 };
+
+// Store identifiers, make sure identifiers with same name is unique
+class String_Tab {
+private:
+  std::vector<Symbol *> *symtab;
+
+public:
+  String_Tab() { this->symtab = new std::vector<Symbol *>; }
+  Symbol *add_string(char *s);
+};
+
+extern String_Tab *id_tab;
+extern String_Tab *str_tab;
+extern String_Tab *int_tab;
 
 #endif
--- src/symtab.cc
+++ src/symtab.cc
@@ -1,9 +1,10 @@
 #include "symtab.h"
 #include <cstring>
 
-Symbol::Symbol(char* s) {
-  int len = (int) strlen(s);  // store length of string without '\0', help to add '\0'
-  str = new char [len+1];
+Symbol::Symbol(char *s) {
+  int len =
+      (int)strlen(s); // store length of string without '\0', help to add '\0'
+  str = new char[len + 1];
   strncpy(str, s, len);
   str[len] = '\0';
 }
@@ -11,3 +12,18 @@ Symbol::Symbol(char* s) {
 bool Symbol::operator==(Symbol other) {
   return std::strcmp(str, other.str) == 0;
 }
+
+Symbol *String_Tab::add_string(char *s) {
+  Symbol *new_sym = new Symbol(s);
+  for (Symbol *sym : *symtab) {
+    if (*new_sym == *sym) { // If find same symbol, return it
+      delete new_sym;
+      return sym;
+    }
+  }
+  return new_sym;
+}
+
+String_Tab *id_tab = new String_Tab();
+String_Tab *str_tab = new String_Tab();
+String_Tab *int_tab = new String_Tab();
