49a15c9e50ee946dee7d4e4f5ac49129bedd678a
Li Haoyuan <1513624626@qq.com>
2024-12-16 13:00:14 +0800

    refactor: Optimize runtime.py, enhance type safety

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -1,23 +1,23 @@
 """
-  Saytring Runtime. A Runtime environment based on Python for Saytring.
-  Copyright (C) 2024 Haoyuan Li
+Saytring Runtime. A Runtime environment based on Python for Saytring.
+Copyright (C) 2024 Haoyuan Li
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from enum import Enum
-from typing import Union, List, cast
+from typing import List, Union, cast
 
 WARN_MSG_STRLEN = 10
 STEP_SKIP_MSG = "Saytring: Step skipped due to type casting error"
@@ -51,7 +51,7 @@ class SaytringVar:
         if self._type == DataType.BOOL:
             return "True" if self._value else "False"
         if self._type == DataType.LIST:
-            return "[" + ", ".join(map(str, self._value)) + "]"
+            return "[" + ", ".join(map(str, cast(list, self._value))) + "]"
         if self._type == DataType.NULL_TYPE:
             return "None"
         return "None"  # Should never reach here
@@ -101,19 +101,19 @@ class SaytringVar:
         return self._str_value
 
     def cast_int(self) -> int:
-        if not self._type is DataType.INT:
+        if self._type is not DataType.INT:
             self.print_warn_msg("Saytring: Try to cast a non-int variable to int")
             raise TypeError
         return cast(int, self._value)
 
     def cast_list(self) -> List[str]:
-        if not self._type is DataType.LIST:
+        if self._type is not DataType.LIST:
             self.print_warn_msg("Saytring: Try to cast a non-list variable to list")
             raise TypeError
         return cast(List[str], self._value)
 
     def cast_bool(self) -> bool:
-        if not self._type is DataType.BOOL:
+        if self._type is not DataType.BOOL:
             self.print_warn_msg("SaytringL Try to cast a non-bool variable to bool")
             raise TypeError
         return cast(bool, self._value)
@@ -149,7 +149,7 @@ def cast_list_to_str(s: SaytringVar, t: SaytringVar) -> None:
 
 
 def cast_null_to_str(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.NULL_TYPE:
+    if s.get_type() is not DataType.NULL_TYPE:
         s.print_warn_msg(
             "Saytring: Try to perform NULL_Type type cast on a non-NULL_Type variable"
         )
@@ -172,47 +172,53 @@ def cast_null_to_str(s: SaytringVar, t: SaytringVar) -> None:
 
 
 def cast_null_to_int(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.NULL_TYPE:
+    if s.get_type() is not DataType.NULL_TYPE:
         s.print_warn_msg(
             "Saytring: Try to perform NULL_Type type cast on a non-NULL_Type variable"
         )
         t.set_value(False)
     # Update _value
     try:
-        s.set_value(int(s.get_value()))
-        t.set_value(True)
+        value = s.get_value()
+        if isinstance(value, (int, str, bool)):
+            s.set_value(int(value))
+            t.set_value(True)
+        else:
+            t.set_value(False)
     except ValueError:
         t.set_value(False)
 
 
 def cast_null_to_bool(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.NULL_TYPE:
+    if s.get_type() is not DataType.NULL_TYPE:
         s.print_warn_msg(
             "Saytring: Try to perform NULL_Type type cast on a non-NULL_Type variable"
         )
         t.set_value(False)
     # Update _value
     result: bool
-    if isinstance(s.get_value(), bool):
-        result = s.get_value()
-    elif isinstance(s.get_value(), str):
+    value = s.get_value()
+    if isinstance(value, bool):
+        result = value
+    elif isinstance(value, str):
         result = (
-            s.get_value() == "True"
-            or s.get_value() == "true"
-            or s.get_value() == "1"
-            or s.get_value() == "t"
-            or s.get_value() == "T"
-            or s.get_value() == "y"
-            or s.get_value() == "Y"
+            value == "True"
+            or value == "true"
+            or value == "1"
+            or value == "t"
+            or value == "T"
+            or value == "y"
+            or value == "Y"
         )
     else:
         t.set_value(False)
         return
     s.set_value(result)
+    t.set_value(True)
 
 
 def cast_str_to_bool(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.STRING:
+    if s.get_type() is not DataType.STRING:
         s.print_warn_msg(
             "Saytring: Try to perform String type cast on a non-String variable"
         )
@@ -245,34 +251,33 @@ def cast_str_to_bool(s: SaytringVar, t: SaytringVar) -> None:
 
 
 def cast_str_to_int(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.STRING:
+    if s.get_type() is not DataType.STRING:
         s.print_warn_msg(
             "Saytring: Try to perform String type cast on a non-String variable"
         )
         t.set_value(False)
     # Update _value
     try:
-        t.set_value(int(s.get_value()))
+        value: str = cast(str, s.get_value())
+        s.set_value(int(value))
         t.set_value(True)
     except ValueError:
         t.set_value(False)
 
 
 def cast_int_to_bool(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.INT:
+    if s.get_type() is not DataType.INT:
         s.print_warn_msg("Saytring: Try to perform Int type cast on a non-Int variable")
         t.set_value(False)
+        return
 
-    if s.get_value > 0:
-        s.set_value(True)
-    else:
-        s.set_value(False)
-
+    value: int = cast(int, s.get_value())
+    s.set_value(value > 0)
     t.set_value(True)
 
 
 def cast_bool_to_int(s: SaytringVar, t: SaytringVar) -> None:
-    if not s.get_type() is DataType.BOOL:
+    if s.get_type() is not DataType.BOOL:
         s.print_warn_msg(
             "Saytring: Try to perform Bool type cast on a non-Bool variable"
         )
@@ -291,111 +296,87 @@ def cast_bool_to_int(s: SaytringVar, t: SaytringVar) -> None:
 #############################################
 
 
+_op_map: dict = {
+    "EQ": lambda x, y: x == y,
+    "NE": lambda x, y: x != y,
+    "LT": lambda x, y: x < y,
+    "LE": lambda x, y: x <= y,
+    "GT": lambda x, y: x > y,
+    "GE": lambda x, y: x >= y,
+}
+
+
 def comp(s1: SaytringVar | str | int, s2: SaytringVar | str | int, op: str) -> bool:
-    t1: int | str
-    t2: int | str
-    if isinstance(s1, str):
-        t1: str = s1
-    elif isinstance(s1, int):
-        t1: int = s1
-    elif isinstance(s1, SaytringVar):
-        if s1.get_type() is DataType.STRING:
-            t1: str = s1.cast_str()
-        elif s1.get_type() is DataType.INT:
-            t1: int = s1.cast_int()
-        else:
-            print(
-                "Saytring: Try to perform comparison operat1on on a non-String/Int variable, return False by default."
-            )
-            return False
+    def _get_value(s: SaytringVar | str | int) -> int | str | None:
+        if isinstance(s, str):
+            return s
+        elif isinstance(s, int):
+            return s
+        elif isinstance(s, SaytringVar):
+            if s.get_type() is DataType.STRING:
+                return s.cast_str()
+            elif s.get_type() is DataType.INT:
+                return s.cast_int()
+            else:
+                print(
+                    "Saytring: Try to perform comparison operation on a non-String/Int variable, return False by default."
+                )
+                return None
 
-    if isinstance(s2, str):
-        t2: str = s2
-    elif isinstance(s2, int):
-        t2: int = s2
-    elif isinstance(s2, SaytringVar):
-        if s2.get_type() is DataType.STRING:
-            t2: str = s2.cast_str()
-        elif s2.get_type() is DataType.INT:
-            t2: int = s2.cast_int()
-        else:
-            print(
-                "Saytring: Try to perform comparison operat2on on a non-String/Int variable, return False by default."
-            )
-            return False
+    t1 = _get_value(s1)
+    t2 = _get_value(s2)
 
-    if (
-        isinstance(t1, int)
-        and isinstance(t2, str)
-        or isinstance(t1, str)
-        and isinstance(t2, int)
-    ):
-        print(
-            "Saytring: Try to perform comparison operation on a String and an Int, return False by default"
-        )
+    if t1 is None or t2 is None:
         return False
 
-    if op == "EQ":
-        return t1 == t2
-    if op == "NE":
-        return t1 != t2
-    if op == "LT":
-        return t1 < t2
-    if op == "LE":
-        return t1 <= t2
-    if op == "GT":
-        return t1 > t2
-    if op == "GE":
-        return t1 >= t2
+    if type(t1) is type(t2):
+        return _op_map[op](t1, t2)
 
-    print("Saytring: Invalid operator, return False by default")
+    print(
+        "Saytring: Try to compare two varibale with different type, return False by default"
+    )
     return False
 
 
 def arithmetic(
     s1: SaytringVar | int | str, s2: SaytringVar | int | str, op: str
 ) -> int | str:
-    # Process strings
-    if (
-        isinstance(s1, str)
-        or (isinstance(s1, SaytringVar) and s1.get_type() is DataType.STRING)
-    ) and (
-        isinstance(s2, str)
-        or (isinstance(s2, SaytringVar) and s2.get_type() is DataType.STRING)
-    ):
+    # Helper function to get value from SaytringVar or direct value
+    def _get_value(s: SaytringVar | str | int) -> int | str | None:
+        if isinstance(s, str):
+            return s
+        elif isinstance(s, int):
+            return s
+        elif isinstance(s, SaytringVar):
+            if s.get_type() is DataType.STRING:
+                return s.cast_str()
+            elif s.get_type() is DataType.INT:
+                return s.cast_int()
+            else:
+                print(
+                    "Saytring: Try to perform arithmetic operation on a non-String/Int variable, return 0 by default."
+                )
+                return None
+
+    t1 = _get_value(s1)
+    t2 = _get_value(s2)
+
+    if t1 is None or t2 is None:
+        return 0
+
+    # Check if both are strings
+    if isinstance(t1, str) and isinstance(t2, str):
         if op == "ADD":
-            return _concat(s1, s2)
+            return _concat(t1, t2)
         if op == "SUB":
-            return _remove_tail(s1, s2)
+            return _remove_tail(t1, t2)
 
-        print("Saytring: Invalid operator for string operation")
-        return ""
-
-    # Process numbers
-    if (
-        isinstance(s1, int)
-        or (isinstance(s1, SaytringVar) and s1.get_type() is DataType.INT)
-    ) and (
-        isinstance(s2, int)
-        or (isinstance(s2, SaytringVar) and s2.get_type() is DataType.INT)
-    ):
-        try:
-            t1: int = s1 if isinstance(s1, int) else s1.cast_int()
-            t2: int = s2 if isinstance(s2, int) else s2.cast_int()
-        except TypeError:
-            print(
-                "Saytring: Try to perform arithmetic operation on non-int variables, return 0 by default"
-            )
-            print(STEP_SKIP_MSG)
-            return 0
-
+    # Check if both are integers
+    if isinstance(t1, int) and isinstance(t2, int):
         if op == "-":
             return t1 - t2
         if op == "+":
             return t1 + t2
-
-        print("Saytring: Invalid operator, return 0 by default")
-        return 0
 
     # Mixed types
     print("Saytring: Cannot perform arithmetic operation between int and string")
@@ -408,12 +389,17 @@ def arithmetic(
 #############################################
 
 
-# reverse(string) : string
 def reverse(s: SaytringVar, t: SaytringVar) -> None:
+    """
+    Reverse the string in 's' and store the result in 't'.
+    """
     t.set_value(s.cast_str()[::-1])
 
 
 def concat(s1: SaytringVar, s2: SaytringVar | str, t: SaytringVar) -> None:
+    """
+    Concatenate the string in 's1' with the string in 's2' and store the result in 't'.
+    """
     try:
         t.set_value(s1.cast_str() + (s2 if isinstance(s2, str) else s2.cast_str()))
     except TypeError:
@@ -431,9 +417,12 @@ def _concat(s1: SaytringVar | str, s2: SaytringVar | str) -> str:
 
 
 def remove_tail(s: SaytringVar, tail: SaytringVar | str, t: SaytringVar) -> None:
-    s: str = s.cast_str()
-    tail: str = tail if isinstance(tail, str) else tail.cast_str()
-    result: str = s[: -len(tail)] if s.endswith(tail) else s
+    """
+    Remove the 'tail' from the end of the string in 's' and store the result in 't'.
+    """
+    s_str: str = s.cast_str()
+    tail_str: str = tail if isinstance(tail, str) else tail.cast_str()
+    result: str = s_str[: -len(tail_str)] if s_str.endswith(tail_str) else s_str
     t.set_value(result)
 
 
@@ -446,6 +435,9 @@ def _remove_tail(s: SaytringVar | str, tail: SaytringVar | str) -> str:
 def substring(
     s: SaytringVar, start: SaytringVar | int, end: SaytringVar | int, t: SaytringVar
 ) -> None:
+    """
+    Extract a substring from 's' starting at 'start' and ending at 'end', and store the result in 't'.
+    """
     # Perform type casting
     try:
         start = start.cast_int() if isinstance(start, SaytringVar) else start
@@ -459,6 +451,9 @@ def substring(
 def substring_from_start(
     s: SaytringVar, end: SaytringVar | int, t: SaytringVar
 ) -> None:
+    """
+    Extract a substring from 's' starting at the beginning and ending at 'end', and store the result in 't'.
+    """
     try:
         end = end.cast_int() if isinstance(end, SaytringVar) else end
         t.set_value(s.cast_str()[:end])
@@ -468,6 +463,9 @@ def substring_from_start(
 
 
 def get_length(s: SaytringVar, t: SaytringVar) -> None:
+    """
+    Get the length of the string in 's' and store the result in 't'.
+    """
     try:
         t.set_value(len(s.cast_str()))
     except TypeError:
@@ -476,6 +474,9 @@ def get_length(s: SaytringVar, t: SaytringVar) -> None:
 
 
 def is_palindrome(s: SaytringVar, t: SaytringVar) -> None:
+    """
+    Check if the string in 's' is a palindrome and store the result in 't'.
+    """
     try:
         str_value = s.cast_str()
         t.set_value(str_value == str_value[::-1])
@@ -485,6 +486,9 @@ def is_palindrome(s: SaytringVar, t: SaytringVar) -> None:
 
 
 def say(s: SaytringVar | str | int | bool) -> None:
+    """
+    Print the value of 's'.
+    """
     try:
         print(s.cast_str() if isinstance(s, SaytringVar) else s)
     except TypeError:
@@ -493,10 +497,16 @@ def say(s: SaytringVar | str | int | bool) -> None:
 
 
 def ask(t: SaytringVar) -> None:
+    """
+    Prompt the user for input and store the result in 't'.
+    """
     t.set_NULL_value(input())
 
 
 def ask_with_prompt(s: SaytringVar | str, t: SaytringVar) -> None:
+    """
+    Prompt the user for input with the prompt 's' and store the result in 't'.
+    """
     try:
         t.set_NULL_value(input(s.cast_str() if isinstance(s, SaytringVar) else s))
     except TypeError:
@@ -609,7 +619,6 @@ def get_at(s: SaytringVar, index: SaytringVar | int, t: SaytringVar) -> None:
         print(f"Saytring: Index error in get_at: {e}")
         print(STEP_SKIP_MSG)
         t.set_NULL_value("")
-
 
 
 #####################################################################################
