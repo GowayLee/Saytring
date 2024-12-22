02fdf5ab68bdc7753033414d755e7359d7ecf574
Li Haoyuan <1513624626@qq.com>
2024-12-22 19:04:58 +0800

    docs: Add License

--- src/include/semant.h
+++ src/include/semant.h
@@ -1,3 +1,20 @@
+/*
+  Saytring Compiler. A compiler translating Saytring to Python.
+  Copyright (C) 2024 Haoyuan Li
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef _SEMANT_H
 #define _SEMANT_H
 
