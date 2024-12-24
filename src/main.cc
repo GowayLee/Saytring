40c8dedd2d057aeef1bc2334381fe7b8a3f3f1fb
Li Haoyuan <1513624626@qq.com>
2024-12-25 02:36:54 +0800

    fix: fix logic of handling escape character

--- src/main.cc
+++ src/main.cc
@@ -15,23 +15,6 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-/*
-  Saytring Compiler. A compiler translating Saytring to Python.
-  Copyright (C) 2024 Haoyuan Li
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 
 #include "AST.h"
 #include "flag_handler.h"
@@ -133,7 +116,8 @@ int main(int argc, char **argv) {
   std::cout << "\nCompilation completed in " << diff.count() << " seconds.\n";
 
   printf("Compilation successful.\n");
-  printf("Ready to go ヾ(≧▽≦*)o\n");
+  // printf("Ready to go ヾ(≧▽≦*)o\n");
+  printf("Ready to go :p\n");
 
   // Run code if --run flag is set to "true"
   if (parsed_flags["--run"] == "true") {
